0a1208b0aaca3443a7ff8dff797e0550835e2fb6
Tim Goloshchapov <tim.goloschapov@gmail.com>
2022-02-11 14:12:32 -0600

    project put done

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,68 +1,106 @@
-const Project = require('../projects/projects-model');
+const Project = require("../projects/projects-model");
 
-const router = require('express').Router()
+const router = require("express").Router();
 
 //-----------------------GET--------------------------------------
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   Project.get()
-    .then(projects => {
-      if(!projects) {
-        res.status(200).json([])
+    .then((projects) => {
+      if (!projects) {
+        res.status(200).json([]);
       } else {
-      res.status(200).json(projects);
+        res.status(200).json(projects);
       }
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       res.status(500).json({
         message: "The projects information could not be retrieved",
-      })
-    })
-})
+      });
+    });
+});
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   Project.get(req.params.id)
-    .then(project => {
+    .then((project) => {
       if (!project) {
-          res.status(404).json({
-            message: "The project with the specified ID does not exist"
-          })
-        } else {
-        res.status(200).json(project)}
+        res.status(404).json({
+          message: "The project with the specified ID does not exist",
+        });
+      } else {
+        res.status(200).json(project);
+      }
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).json({
         message: "The post information could not be retrieved",
-        error: err.message
-      })
-    })
-})
+        error: err.message,
+      });
+    });
+});
 
 //-----------------------POST--------------------------------------
 
-router.post('/', (req, res) => {
-  const { description, name, completed} = req.body;
-  if( !description || !name) {
+router.post("/", (req, res) => {
+  const { description, name, completed } = req.body;
+  if (!description || !name) {
     res.status(400).json({
-      message: "Please provide all of the info for the post"
-    })
+      message: "Please provide all of the info for the post",
+    });
   } else {
     Project.insert({ description, name, completed })
-      .then(({id})=>{
-        return Project.get(id)
+      .then(({ id }) => {
+        return Project.get(id);
       })
-      .then(newProject => {
-        res.status(201).json(newProject)
+      .then((newProject) => {
+        res.status(201).json(newProject);
       })
-      .catch(err => {
+      .catch((err) => {
         res.status(500).json({
-          message: "There was an error while saving the project to the database",
-          error: err.message
-        })
-      })
+          message:
+            "There was an error while saving the project to the database",
+          error: err.message,
+        });
+      });
   }
-})
+});
 
+//---------------------------PUT--------------------------------
+
+router.put("/:id", (req, res) => {
+  const { description, name, completed } = req.body;
+  if (!description || !name || completed === undefined) {
+    res.status(400).json({
+      message: "Please provide description, name, and completion for the post",
+    });
+  } else {
+    Project.get(req.params.id)
+      .then((project) => {
+        if (!project) {
+          res.status(404).json({
+            message: "The project with the specified ID does not exist",
+          });
+        } else {
+          return Project.update(req.params.id, req.body);
+        }
+      })
+      .then((info) => {
+        if (info) {
+          return Project.get(req.params.id);
+        }
+      })
+      .then((project) => {
+        res.status(201).json(project);
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message:
+            "There was an error while saving the project to the database",
+          error: err.message,
+        });
+      });
+  }
+});
 
 module.exports = router;
