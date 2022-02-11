6bc65f38a5c182a6fcef9c77d1b99039c48f9b11
Tim Goloshchapov <tim.goloschapov@gmail.com>
2022-02-11 16:53:52 -0600

    all tests are green, need to test actions, edit action POST, check middleware

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -39,4 +39,88 @@ router.get("/:id", (req, res) => {
     });
 });
 
+//----------------------POST-------------------------
+
+router.post("/", (req, res) => {
+  const { project_id, description, notes } = req.body;
+  if (!project_id || !description || !notes) {
+    res.status(400).json({
+      message: "Please provide all of the info for the action",
+    });
+  } else {
+    Action.insert({ project_id, description, notes })
+      .then(({ id }) => {
+        return Action.get(id);
+      })
+      .then((newAction) => {
+        res.status(201).json(newAction);
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message:
+            "There was an error while saving the action to the database",
+          error: err.message,
+        });
+      });
+  }
+});
+
+//------------------------PUT-------------------------------
+
+router.put("/:id", (req, res) => {
+  const { project_id, description, notes } = req.body;
+  if (!project_id || !description || !notes ) {
+    res.status(400).json({
+      message: "Please provide all of the info for the action",
+    });
+  } else {
+    Action.get(req.params.id)
+      .then((action) => {
+        if (!action) {
+          res.status(404).json({
+            message: "The action with the specified ID does not exist",
+          });
+        } else {
+          return Action.update(req.params.id, req.body);
+        }
+      })
+      .then((info) => {
+        if (info) {
+          return Action.get(req.params.id);
+        }
+      })
+      .then((action) => {
+        res.status(201).json(action);
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message:
+            "There was an error while saving the action to the database",
+          error: err.message,
+        });
+      });
+  }
+});
+
+//----------------------DELETE--------------------------
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const action = await Action.get(req.params.id);
+    if (!action) {
+      res.status(404).json({
+        message: "The action with the specified ID does not exist",
+      });
+    } else {
+      await Action.remove(req.params.id);
+      res.status(201).json();
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "The action could not be removed",
+    });
+  }
+});
+
 module.exports = router;
