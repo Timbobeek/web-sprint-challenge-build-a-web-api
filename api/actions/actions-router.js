052c1efe0a8d5543a22f7474c0ab0f0e2974a1f4
Tim Goloshchapov <tim.goloschapov@gmail.com>
2022-02-11 16:33:36 -0600

    action getters done

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -5,12 +5,12 @@ const router = require('express').Router()
 //-----------------------GET--------------------------------------
 
 router.get('/', (req, res) => {
-  Action.get(req.query)
+  Action.get()
     .then(actions => {
       if(!actions) {
-        res.json([])
-      }
-      res.status(200).json(actions);
+        res.status(200).json([]);
+      } else {
+        res.status(200).json(actions);}
     })
     .catch(err => {
       console.log(err);
@@ -19,5 +19,24 @@ router.get('/', (req, res) => {
       })
     })
 })
+
+router.get("/:id", (req, res) => {
+  Action.get(req.params.id)
+    .then((action) => {
+      if (!action) {
+        res.status(404).json({
+          message: "The action with the specified ID does not exist",
+        });
+      } else {
+        res.status(200).json(action);
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "The action information could not be retrieved",
+        error: err.message,
+      });
+    });
+});
 
 module.exports = router;
